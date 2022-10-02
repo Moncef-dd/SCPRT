96210088f571b7ca7c1790da54a71728a1f133d7
walidksb <walid.kesbi@gmail.com>
2022-10-02 09:37:13 +0100

    making statment on this animation

--- src/components/ImagesMerge.js
+++ src/components/ImagesMerge.js
@@ -2,62 +2,51 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+// this is a renderable animated component to animate the styling content process
 export const Images = () => {
-    return ( <
-        div className = "anim" >
-        <
-        motion.div className = "containerr container1"
-        initial = {
-            { opacity: 1, x: -500 } }
-        animate = {
-            {
-                opacity: 0.5,
-                x: 0,
-                transition: {
-                    duration: 1,
-                },
-            }
-        }
-        transition = {
-            {
-                type: "spring",
-                stiffness: 260,
-                damping: 20,
-            }
-        }
-        style = {
-            { display: "absolute" } }
-        />{" "} <
-        motion.div className = "containerr container2"
-        initial = {
-            { opacity: 1, x: 500 } }
-        animate = {
-            {
-                opacity: 0,
-                x: 0,
-                transition: {
-                    duration: 1,
-                },
-            }
-        }
-        transition = {
-            {
-                type: "spring",
-                stiffness: 260,
-                damping: 20,
-            }
-        }
-        onAnimationComplete = {
-            () => {
-                document.querySelector(".container1").className =
-                    "containerr container3";
-                document.querySelector(".container2").className =
-                    "containerr container3";
-                document.querySelector(".container2").style.opacity = 1;
-                document.querySelector(".container1").style.opacity = 1;
-            }
-        }
-        />{" "} <
-        /div>
-    );
+	return (
+		<div className="anim">
+			<motion.div
+				className="containerr container1"
+				initial={{ opacity: 1, x: -500 }}
+				animate={{
+					opacity: 0.5,
+					x: 0,
+					transition: {
+						duration: 1,
+					},
+				}}
+				transition={{
+					type: "spring",
+					stiffness: 260,
+					damping: 20,
+				}}
+				style={{ display: "absolute" }}
+			/>{" "}
+			<motion.div
+				className="containerr container2"
+				initial={{ opacity: 1, x: 500 }}
+				animate={{
+					opacity: 0,
+					x: 0,
+					transition: {
+						duration: 1,
+					},
+				}}
+				transition={{
+					type: "spring",
+					stiffness: 260,
+					damping: 20,
+				}}
+				onAnimationComplete={() => {
+					document.querySelector(".container1").className =
+						"containerr container3";
+					document.querySelector(".container2").className =
+						"containerr container3";
+					document.querySelector(".container2").style.opacity = 1;
+					document.querySelector(".container1").style.opacity = 1;
+				}}
+			/>{" "}
+		</div>
+	);
 };
